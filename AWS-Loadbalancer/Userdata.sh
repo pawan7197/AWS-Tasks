fb20b1eeb465f43f8512d59d795e92657bf29d9f
pawan7197 <p.kothapalli@ajacs.in>
2025-09-03 11:55:06 +0530

    Update Userdata.sh

--- AWS-Loadbalancer/Userdata.sh
+++ AWS-Loadbalancer/Userdata.sh
@@ -3,13 +3,5 @@ sudo apt -y update
 sudo apt -y install nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-
- 
-#Install git - client for downloading code from GitHub
- 
-sudo apt -y install git
-echo "git installed"
 sudo rm -rf /var/www/html*
-echo "file is removed"
 sudo git clone https://github.com/Ai-TechNov/mario.git /var/www/html/
-echo "new app cloned"
